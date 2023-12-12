0e6f105e1f605d01154177424dcce06ece88d7b7
Priyanshu <abpriyanshu007@gmail.com>
2023-12-12 07:37:27 +0530

    Update Font Style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter, Tilt_Prism, Borel } from 'next/font/google'
+import { Inter,  } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
 
-const tilt_prism = Tilt_Prism({
-  subsets: ['latin'],
-  variable: '--font-tilt-prism',
-  // display: 'swap',
-})
-
 const inter = Inter({ 
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap',
+  subsets: ['latin']
 })
-
-
-const borel = Borel({
-  subsets: ['latin'],
-  variable: '--font-borel',
-  display: 'swap',
-  weight: '400'
-}) 
 
 export const metadata: Metadata = {
   title: 'My Portfolio',
@@ -34,10 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`
-      ${inter.className} 
-      `}>
-
+    <html lang="en">
       <body>
         {children}
         </body>
