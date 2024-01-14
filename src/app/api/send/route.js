fd1c0a226cb4059b1a30d4b2d6824c4b01c716eb
Priyanshu <abpriyanshu007@gmail.com>
2024-01-14 12:27:32 +0530

    Commented out unused code in send route

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,25 +1,25 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
-// import { renderToReadableStream } from 'react-dom/server';
+// // import { EmailTemplate } from '../../../components/EmailTemplate';
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
+// // import { renderToReadableStream } from 'react-dom/server';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: "",
-      to: [""],
-      subject: "Hello world",
-      react: (
-        <>
-          <p>Email Body</p>
-        </>
-      ),
-    });
+// // export async function POST() {
+// //   try {
+// //     const data = await resend.emails.send({
+// //       from: "",
+// //       to: [""],
+// //       subject: "Hello world",
+// //       react: (
+// //         <>
+// //           <p>Email Body</p>
+// //         </>
+// //       ),
+// //     });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+// //     return NextResponse.json(data);
+// //   } catch (error) {
+// //     return NextResponse.json({ error });
+// //   }
+// // }
