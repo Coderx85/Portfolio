6c8682ad71b40b1e57e1bd6ee056cad2df7fea39
Priyanshu <abpriyanshu007@gmail.com>
2024-01-26 00:12:42 +0530

    Fix hovering issue in Navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
 
+// Fix hovering issue
+
 const navLinks = [
   {
     title: "About",
