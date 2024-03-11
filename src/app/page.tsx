91a5a9f744ea3b6b41e6b326301abd88a61d7da0
Priyanshu <abpriyanshu007@gmail.com>
2024-03-11 20:17:57 +0530

    Refactor page component structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,17 @@ import ProjectSection from "./Components/ProjectSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar/>
-      <div className="container mx-auto px-12 py-4">
-        <HeroSection />
-        <AboutSection />
-        <ProjectSection />
-        {/* <EmailSection /> */}
-      </div>
-        <Footer />
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+        <Navbar/>
+        <div className="container mx-auto px-12 py-4">
+          <HeroSection />
+          <AboutSection />
+          <ProjectSection />
+          {/* <EmailSection /> */}
+        </div>
+          <Footer />
+      </main>
+    </>
   )
 }
