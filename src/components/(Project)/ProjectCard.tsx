7edf460ebd68a03f890cdf0d4eccf4c734a053b0
Priyanshu085 <abpriyanshu007@gmail.com>
2024-06-08 02:35:50 +0530

    refactor: Update file paths and imports for renamed components

--- src/components/(Project)/ProjectCard.tsx
+++ src/components/(Project)/ProjectCard.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react';
-import { ProjectCardProp } from '../types';
+import { ProjectCardProp } from '@/types';
 import ProjectDetails from './ProjectDetails';
 
 interface ProjectCardProps {
