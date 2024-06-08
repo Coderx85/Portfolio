894ffc009d38c07279170a4933dd97bc96049202
Priyanshu085 <abpriyanshu007@gmail.com>
2024-06-09 04:00:24 +0530

    refactor: Update EmailSection component social links styling and structure

--- src/components/(footer)/EmailSection.tsx
+++ src/components/(footer)/EmailSection.tsx
@@ -57,7 +57,8 @@ const EmailSection = () => {
               className="rounded-full cursor-pointer hover:scale-120 transform transition duration-300 ease-in-out"
             /> */}
             <div className="group relative">
-              <span className="flex items-center justify-center w-16 h-16 border-2 border-white bg-black text-white rounded-none group-hover:rounded-full group-hover:bg-white group-hover:text-black transition-all duration-300 ease-in-out">
+              <span className="flex items-center justify-center w-80 h-80 border-2 border-white bg-black text-white rounded-none group-hover:rounded-full
+               group-hover:bg-white group-hover:text-black transition-all duration-1000 ease-in-out">
                 {link.icon}
               </span>
             </div>
