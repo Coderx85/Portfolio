968d170fd7e3725145bc048880afcdb821326f50
Priyanshu085 <abpriyanshu007@gmail.com>
2024-06-09 00:01:30 +0530

    refactor: Update AboutSection component to use responsive font size in About text

--- src/components/(about)/AboutSection.tsx
+++ src/components/(about)/AboutSection.tsx
@@ -51,7 +51,7 @@ const AboutSection = () => {
                 '>
                     About Me</h2>
                 <div className=' border-4 primary-bd py-7 px-7 rounded-3xl' id='about-text'>
-                    <p className='sm:text-xl text-xs'>
+                    <p className='md:text-xl text-xs'>
                         Hey there! I&apos;m Priyanshu, a 3rd year engineering student with a focus on Data Science. 
                         Proficient in Python, MERN stack, and front-end technologies, I&apos;ve embarked on a journey to create impactful tech solutions. 
                         Currently, I&apos;m diving into Generative AI and Prompt Engineering, aiming to fuse these technologies into innovative projects.
