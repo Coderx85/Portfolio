415c6dae6bee8a236266b7aee9cfb7eddbfe5a55
Coderx85 <work.priyanshu085@gmail.com>
2025-04-23 00:44:24 +0530

    Refactor code structure for improved readability and maintainability

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { hostname } = require("os")
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'avatars.githubusercontent.com',
+        protocol: 'https',
+      },
+      {
+        hostname: 'opengraph.githubassets.com',
+        protocol: 'https',
+      },{
+        hostname: 'img.shields.io',
+        protocol: 'https',
+      }
+    ],
+  },
+}
 
 module.exports = nextConfig
